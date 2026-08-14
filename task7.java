f8bb4aebb875e09d39c38017ebc211d73aef1bad
Y.Lakshmi Narasimha <vtu28209@veltech.edu.in>
2026-08-14 11:50:43 +0530

    Add files via upload

---
+++ task7.java
@@ -0,0 +1,8 @@
+public class task7 {
+    public static void main(String[] args) {
+        double celsius = 30;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        
+        System.out.println(celsius + " Celsius = " + fahrenheit + " Fahrenheit");
+    }
+}
