95c9a9959c22a9aea3c70b16cd07bdad4138cd9a
Y.Lakshmi Narasimha <vtu28209@veltech.edu.in>
2026-08-27 12:07:17 +0530

    Add files via upload

---
+++ week 1/task8.java
@@ -0,0 +1,13 @@
+public class task8 {
+    public static void main(String[] args) {
+        String text = "Hello";
+        String reversed = "";
+        
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
+        }
+        
+        System.out.println("Original: " + text);
+        System.out.println("Reversed: " + reversed);
+    }
+}
