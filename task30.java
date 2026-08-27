e87e0856b033cd01c7015f14f5b5365e8d481f2b
Y.Lakshmi Narasimha <vtu28209@veltech.edu.in>
2026-08-27 12:05:18 +0530

    Delete task30.java

--- task30.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class task30 {
-    public static void main(String[] args) {
-        int[] numbers = {5, 2, 8, 1, 3};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-    }
-}
