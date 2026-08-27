eda9affaab4a1fcb14b4cf318571d32f5ab26891
Y.Lakshmi Narasimha <vtu28209@veltech.edu.in>
2026-08-27 12:03:55 +0530

    Delete task23.java

--- task23.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class task23 {
-    public static void main(String[] args) {
-        LocalDate date = LocalDate.of(2024, 12, 31);
-        System.out.println("Day of year: " + date.getDayOfYear());
-    }
-}
